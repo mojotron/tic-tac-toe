99ab5d9476e9e7a50ebf005fba390bf17fd21f93
mojotron <igormalj@gmail.com>
2018-07-06 09:03:18 +0200

    Refactor and clean

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,42 +1,28 @@
 class TicTacToe
-	attr_reader :moves, :player1, :player2, :player1_score, :player2_score
+	#special tnx to my nephews Eric, Adrijan, Bruno,
+	#without your testing this project would be much less awesome :p
+	attr_reader :board, :player1, :player2
 
 	def initialize
 		@board = Board.new
 		@player1 = Player.new
-		@player2 = Player.new
-		@player1_score = 0
-		@player2_score = 0
-		@moves = {
-			1 => [0,0], 2 => [0,1], 3 => [0,2],
-			4 => [1,0], 5 => [1,1], 6 => [1,2],
-			7 => [2,0], 8 => [2,1], 9 => [2,2]
-		}	
+		@player2 = Player.new	
 	end
 
-	def display_board
-		@board.new_board.each { |line| puts "\t#{line}" }
+	def display_board #display board array line by line
+		@board.new_board.each { |line| puts "\t#{line.to_s.gsub('"','').gsub(',', ' |')}" }
 	end
 
 	def player_move(token, board, move)
-		raise MyError if board[move[0]][move[1]] == "\u2718" ||
+		#raise error if player trys reassign position
+		raise MyAssignementError if board[move[0]][move[1]] == "\u2718" ||
 			board[move[0]][move[1]] == "\u25CF"
-	
-			board[move[0]][move[1]] = token
+		#place token on wanted position	
+		board[move[0]][move[1]] = token
 	end
 
-	def winner_found?(token, board)
-		win_positions = {
-			1 => [moves[1],moves[2],moves[3]],
-			2 => [moves[4],moves[5],moves[6]],
-			3 => [moves[7],moves[8],moves[9]],
-			4 => [moves[1],moves[4],moves[7]],
-			5 => [moves[2],moves[5],moves[8]],
-			6 => [moves[3],moves[6],moves[9]],
-			7 => [moves[1],moves[5],moves[9]],
-			8 => [moves[3],moves[5],moves[7]]
-		}
-		winner_found = false
+	def winner_found?(token, board, win_positions) #check for 1 of 8 winning posibilitys
+		winner_found = false #flag variable
 		win_positions.each do |key, value|
 			win = []
 			value.each do |item|
@@ -50,17 +36,17 @@ class TicTacToe
 
 	def start_game
 		display_board
-		i = 1
-		while i <= 9
+		9.times do |i| #9 position, so 9 possible turns
 			current_player = ''
 			# change players turns vith odd/even numbers
-			i % 2 != 0 ? current_player = @player1 : current_player = @player2
-			puts "<< Turn #{i} >>"
+			(i+1) % 2 != 0 ? current_player = @player1 : current_player = @player2
+			puts "<< Turn #{i + 1} >>"
 			print "#{current_player.username} #{current_player.user_token} enter "
 			print "position number (1-9) >> "
+			#catch errors, invalid input(non digit), and reassignement
 			begin
 				player_move = gets.chomp.to_i
-				player_move(current_player.user_token, @board.new_board, @moves[player_move])
+				player_move(current_player.user_token, @board.new_board, @board.moves[player_move])
 			rescue NoMethodError
 				print "Invalid input, enter digit 1-9 for target position! >>"
 				retry
@@ -68,44 +54,48 @@ class TicTacToe
 				puts "Invalid input position already assign! Try again! >>"
 				retry
 			end
-			win = winner_found?(current_player.user_token, @board.new_board)
+			win = winner_found?(current_player.user_token, @board.new_board, @board.win_positions)
 			display_board
 			if win == true
-				current_player == @player1 ? @player1_score += 1 : @player2 += 1
+				current_player.score += 1
 				puts "PLAYER #{current_player.username} WON!"
-				puts "#{@player1.username} #{@player1_score} : #{@player2.username} #{@player2_score}"
+				score_board_display
 				new_game
 			end
-			i += 1
 		end
 		puts "DRAW !!!"
-		puts "#{@player1.username} #{@player1_score} : #{@player2.username} #{@player2_score}"
+		score_board_display
 		new_game
 	end
 
 	def new_game
 		@board = Board.new #reset map
 		@player1, @player2 = @player2, @player1#new game players swap turns
-		@player1_score, @player2_score = @player2_score, @player1_score 
 		puts "New game : type y/n"
 		input = gets.chomp
 		input == 'y' ? start_game : exit	
 	end
+
+	def score_board_display
+		puts "#{@player1.username} #{@player1.score} : #{@player2.username} #{@player2.score}"
+	end
 end
 
 class Player
-	attr_reader :username, :user_token
-	@@token = ["\u2718", "\u25CF"].shuffle
+	attr_reader :username, :user_token, :score
+	attr_writer :score
+	@@token = ["\u2718", "\u274d"].shuffle
 
 	def initialize
 		print "Enter your username >> "
 		@username = gets.chomp
 		@user_token = @@token.pop
+		@score = 0
 	end
 end
 
 class Board
-	attr_reader :new_board
+	attr_reader :new_board, :moves, :win_positions
 
 	def initialize
 		@new_board = [
@@ -113,10 +103,25 @@ class Board
 			["\u2783","\u2784","\u2785"],
 			["\u2786","\u2787","\u2788"]
 		]
+		@moves = {
+			1 => [0,0], 2 => [0,1], 3 => [0,2],
+			4 => [1,0], 5 => [1,1], 6 => [1,2],
+			7 => [2,0], 8 => [2,1], 9 => [2,2]
+		}
+		@win_positions = {
+			1 => [moves[1],moves[2],moves[3]],
+			2 => [moves[4],moves[5],moves[6]],
+			3 => [moves[7],moves[8],moves[9]],
+			4 => [moves[1],moves[4],moves[7]],
+			5 => [moves[2],moves[5],moves[8]],
+			6 => [moves[3],moves[6],moves[9]],
+			7 => [moves[1],moves[5],moves[9]],
+			8 => [moves[3],moves[5],moves[7]]
+		}
 	end
 end
 
-class MyError < StandardError;
+class MyAssignementError < StandardError;
 end
 
 game = TicTacToe.new
