8b604c47faa08e3db26c41cab40fd4f51f35b66c
mojotron <igormalj@gmail.com>
2018-07-07 08:32:54 +0200

    Update readme, add docs

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -7,27 +7,6 @@ class TicTacToe
 		@player2 = Player.new	
 	end
 
-	def player_move(token, board, move)
-		#raise error if player trys reassign position
-		raise DoubleAssignementError if board[move[0]][move[1]] == "\u2718" ||
-			board[move[0]][move[1]] == "\u274d"
-		#place token on wanted position	
-		board[move[0]][move[1]] = token
-	end
-
-	def winner_found?(token, board, win_positions) #check for 1 of 8 winning posibilitys
-		winner_found = false #flag variable
-		win_positions.each do |key, value|
-			win = []
-			value.each do |item|
-				board[item[0]][item[1]] == token ? win.push(true) :
-					win.push(false)
-			end
-			winner_found = true if win.all? {|x| x == true}
-		end
-		winner_found
-	end
-
 	def start_game
 		@board.display_board
 		9.times do |i| #9 position, for 9 possible turns
@@ -45,7 +24,7 @@ class TicTacToe
 				print "Invalid input, enter digit 1-9 for target position! >>"
 				retry
 			rescue DoubleAssignementError
-				puts "Invalid input position already assign! Try again! >>"
+				puts "Invalid input position already assign! Try again! >> "
 				retry
 			end
 			win = winner_found?(current_player.user_token, @board.new_board, @board.win_positions)
@@ -59,6 +38,28 @@ class TicTacToe
 	end
 
 	private
+	def winner_found?(token, board, win_positions) #check for 1 of 8 winning posibilitys
+		winner_found = false #flag variable
+		win_positions.each do |key, value|
+			win = []
+			value.each do |item|
+				board[item[0]][item[1]] == token ? win.push(true) :
+					win.push(false)
+			end
+			winner_found = true if win.all? {|x| x == true}
+		end
+		winner_found
+	end
+
+
+	def player_move(token, board, move)
+		#raise error if player trys reassign position
+		raise DoubleAssignementError if board[move[0]][move[1]] == @player1.user_token ||
+			board[move[0]][move[1]] == @player2.user_token
+		#place token on wanted position	
+		board[move[0]][move[1]] = token
+	end
+
 	def end_game_log(message)
 		puts message
 		score_board_display
@@ -82,12 +83,13 @@ class Player
 	attr_reader :username, :user_token, :score
 	attr_writer :score
 	@@token = ["\u2718", "\u274d"].shuffle
-
+	@@player_count = 1 #class count variable
 	def initialize
-		print "Enter your username >> "
+		print "Player #{@@player_count} enter your username >> "
 		@username = gets.chomp
 		@user_token = @@token.pop
 		@score = 0
+		@@player_count += 1
 	end
 end
 
