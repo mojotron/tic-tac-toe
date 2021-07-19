1d2c57fe99ad5d7adcfab01338efb6952e85139c
mojotron <igormalj@gmail.com>
2021-07-19 17:53:49 +0200

    Refactor game engine, add welcome options

--- JS/main.js
+++ JS/main.js
@@ -1,7 +1,6 @@
 "use strict";
 const boardFactory = function () {
   const board = Array.from({ length: 9 }, () => null);
-
   const printBoard = () => board.forEach((ele) => console.log(ele));
   const boardFull = () => !board.includes(null);
   const getMarketAt = function (i) {
@@ -12,7 +11,7 @@ const boardFactory = function () {
     if (i < 0 || i > 9) return;
     board[i] = marker;
   };
-  const checkWinCondition = function (marker) {
+  const winCheck = function (marker) {
     const winnerLines = [
       [0, 1, 2],
       [3, 4, 5],
@@ -28,7 +27,7 @@ const boardFactory = function () {
     }
     return false;
   };
-  return { printBoard, boardFull, getMarketAt, setMarkerAt, checkWinCondition };
+  return { printBoard, boardFull, getMarketAt, setMarkerAt, winCheck };
 };
 
 const playerFactory = function (nick, marker) {
@@ -38,26 +37,34 @@ const playerFactory = function (nick, marker) {
 };
 
 const ticTacToeFactory = function (player1, marker1, player2, marker2) {
+  //Set up elements
   const board = boardFactory();
   const p1 = playerFactory(player1, marker1);
   const p2 = playerFactory(player2, marker2);
+  const gameEngine = function (event) {
+    const targetSquare = event.target.dataset.square;
+    if (board.getMarketAt(targetSquare)) return; //Guard clause when square already selected
+    event.target.textContent = currentPlayer.getMarker(); //Display marker on the DOM board
+    board.setMarkerAt(targetSquare, currentPlayer.getMarker()); //Set marker to js board object
+    //Check for win
+    if (board.winCheck(currentPlayer.getMarker())) {
+      alert(`WINNER IS ${currentPlayer.getNick()}`);
+      return;
+    }
+    //Check for draw (map is full)
+    if (board.boardFull()) {
+      alert("DRAW");
+      return;
+    }
+    //No win solution swap current player
+    currentPlayer = currentPlayer === p1 ? p2 : p1;
+  };
   //Decide who goes first for round 2 then alternate between rounds
   let currentPlayer = p1;
   const squares = document.querySelectorAll(".board-square");
   for (let square of squares) {
-    square.addEventListener("click", function (e) {
-      const square = e.target.dataset.square;
-      if (board.getMarketAt(square)) return;
-      e.target.textContent = currentPlayer.getMarker();
-      board.setMarkerAt(square, currentPlayer.getMarker());
-      //Check win condition or
-      if (board.checkWinCondition(currentPlayer.getMarker()))
-        alert(`WINNER IS ${currentPlayer.getNick()}`);
-      //If board is full and no win condition its draw
-      if (board.boardFull()) alert("DRAW");
-      currentPlayer = currentPlayer === p1 ? p2 : p1;
-    });
+    square.addEventListener("click", (e) => gameEngine(e));
   }
 };
 
-const newGame = ticTacToeFactory("Neo", "🚀", "Rusty", "🐶");
+const newGame = ticTacToeFactory("Neo", "x", "Rusty", "o");
