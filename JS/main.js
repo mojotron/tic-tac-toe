7058d0d16dac47a0801826bc3f3d438ddcd3ecab
mojotron <igormalj@gmail.com>
2021-07-21 17:28:50 +0200

    Refactor code (DRY) - add hover effect to current player marker

--- JS/main.js
+++ JS/main.js
@@ -36,20 +36,27 @@ const playerFactory = function (nick, marker) {
   return { getNick, getMarker };
 };
 
-const TicTacToe = (function () {
+(function () {
+  //DOM element selectors
   const modal = document.querySelector(".new-game-select");
   const newGameBtn = document.querySelector(".btn-new-game");
   const newRoundBtn = document.querySelector(".btn-new-round");
+  //TODO AI btn
   const squares = document.querySelectorAll(".board-square");
   const left = document.querySelector(".left-player");
   const right = document.querySelector(".right-player");
   const scoreBoard = document.querySelector(".score-board");
   const leftScore = document.querySelector(".left-score");
   const rightScore = document.querySelector(".right-score");
-
+  //Variable declaration
   let board = boardFactory();
   const p1 = playerFactory("player1", "x");
   const p2 = playerFactory("player2", "o");
+  let currentPlayer = p1;
+  let p1Score = 0;
+  let p2Score = 0;
+  //Helper functions
+  const swapPlayer = () => (currentPlayer = currentPlayer === p1 ? p2 : p1);
 
   const currentPlayerEffect = function () {
     if (currentPlayer === p1) {
@@ -60,42 +67,6 @@ const TicTacToe = (function () {
       right.classList.add("right-player-effect");
     }
   };
-
-  let currentPlayer = p1;
-  let p1Score = 0;
-  let p2Score = 0;
-  const gameEngine = function (event) {
-    const targetSquare = event.target.dataset.square;
-    if (board.getMarketAt(targetSquare)) return; //Guard clause when square already selected
-    event.target.textContent = currentPlayer.getMarker(); //Display marker on the DOM board
-    board.setMarkerAt(targetSquare, currentPlayer.getMarker()); //Set marker to js board object
-    //Check for win
-    if (board.winCheck(currentPlayer.getMarker())) {
-      displayMsg(`${currentPlayer.getMarker().toUpperCase()} won the round!`);
-      currentPlayer === p1 ? p1Score++ : p2Score++;
-      scoreBoard.classList.remove("none");
-
-      leftScore.textContent = p1Score;
-      rightScore.textContent = p2Score;
-      newRoundBtn.classList.remove("none");
-      modal.classList.remove("hidden");
-      return;
-    }
-    //Check for draw (map is full)
-    if (board.boardFull()) {
-      alert("DRAW");
-      return;
-    }
-    //No win solution swap current player
-    currentPlayer = currentPlayer === p1 ? p2 : p1;
-    currentPlayerEffect();
-  };
-  //Decide who goes first for round 2 then alternate between rounds
-
-  for (let square of squares) {
-    square.addEventListener("click", (e) => gameEngine(e));
-  }
-
   const newBoard = function () {
     board = boardFactory();
     for (let square of squares) square.textContent = "";
@@ -105,20 +76,68 @@ const TicTacToe = (function () {
     const element = document.querySelector(".option-msg");
     element.textContent = msg;
   };
-  //ON NEW GAME RESET ALL
-  newGameBtn.addEventListener("click", function (e) {
+  const setUpNewGame = function () {
     modal.classList.add("hidden");
     currentPlayerEffect();
     newBoard();
     p1Score = 0;
     p2Score = 0;
     scoreBoard.classList.add("none");
-  });
-  newRoundBtn.addEventListener("click", function (e) {
+  };
+  const setUpNewRound = function () {
     modal.classList.add("hidden");
-    currentPlayer = currentPlayer === p1 ? p2 : p1;
+    swapPlayer();
     currentPlayerEffect();
     newBoard();
-  });
-  //ON NEW ROUND RESET BOARD
+  };
+  const endRoundUpdate = function () {
+    scoreBoard.classList.remove("none");
+    leftScore.textContent = p1Score;
+    rightScore.textContent = p2Score;
+    newRoundBtn.classList.remove("none");
+    modal.classList.remove("hidden");
+  };
+  //Main game engine
+  const gameEngine = function (event) {
+    const targetSquare = event.target.dataset.square;
+    if (board.getMarketAt(targetSquare)) return; //Guard clause when square already selected
+    event.target.textContent = currentPlayer.getMarker(); //Display marker on the DOM board
+    board.setMarkerAt(targetSquare, currentPlayer.getMarker()); //Set marker to js board object
+    //Check for win
+    if (board.winCheck(currentPlayer.getMarker())) {
+      displayMsg(`${currentPlayer.getMarker().toUpperCase()} won the round!`);
+      currentPlayer === p1 ? p1Score++ : p2Score++;
+      endRoundUpdate();
+      return;
+    }
+    //Check for draw (map is full)
+    if (board.boardFull()) {
+      displayMsg(`draw`);
+      endRoundUpdate();
+      return;
+    }
+
+    //No win solution swap current player
+    swapPlayer();
+    currentPlayerEffect();
+    event.target.style.color = "rgba(0, 0, 0, 0.8)";
+  };
+  //Set event listener to DOM boar squares
+  for (let square of squares) {
+    square.addEventListener("click", (e) => gameEngine(e));
+    square.addEventListener("mouseenter", function (e) {
+      if (!board.getMarketAt(e.target.dataset.square)) {
+        e.target.style.color = "rgba(0, 0, 0, 0.35)";
+        e.target.textContent = currentPlayer.getMarker();
+      }
+    });
+    square.addEventListener("mouseleave", function (e) {
+      if (!board.getMarketAt(e.target.dataset.square)) {
+        e.target.textContent = "";
+      }
+    });
+  }
+
+  newGameBtn.addEventListener("click", setUpNewGame);
+  newRoundBtn.addEventListener("click", setUpNewRound);
 })();
